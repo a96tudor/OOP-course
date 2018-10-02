c1d29a99158f8aa0afee66176ef9d1bab94e87ce
Tudor Avram <a96tudor@gmail.com>
2018-10-02 12:55:32 +0300

    finished examples

--- src/exemple/exceptii/ExempluIndexOutOfBounds.java
+++ src/exemple/exceptii/ExempluIndexOutOfBounds.java
@@ -4,12 +4,12 @@ public class ExempluIndexOutOfBounds {
 
     public static void main(String[] args) {
         int[] v = new int[10];
-
         try {
             System.out.println(v[11]);
-        } catch (ArrayIndexOutOfBoundsException exp) {
-            System.out.println("Am prins exceptia!");
+        } catch (IndexOutOfBoundsException exp) {
+            System.out.println("am prins exceptia!");
         }
+
 
     }
 
--- src/exemple/exceptii/ExempluNotaInvalida.java
+++ src/exemple/exceptii/ExempluNotaInvalida.java
@@ -2,8 +2,8 @@ package exemple.exceptii;
 
 public class ExempluNotaInvalida {
 
-    public static void main(String[] args) throws InvalidGradeException {
-        Elev elev = new Elev("Gigel",5);
+    public static void main(String[] args) throws InvalidGradeException{
+        Elev elev = new Elev("Gigel",15);
     }
 
 }
